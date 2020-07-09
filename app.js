c8a92a22aa3a376cd66511198f17ad0a51826241
Muhammad Azhar <60821627+azharkhan1@users.noreply.github.com>
2020-07-10 00:18:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+
+// var count=0;
+// var interval;
+
+// function timer()
+// {
+        
+//         count++;
+//        console.log(count);
+
+// }
+
+
+// interval = setInterval(timer,1000)
+
+// setTimeout(function() {
+//     clearInterval (interval);
+// }
+// ,5000)
+
+// function timeOut(){
+//     console.log("running...");
+// }
+
+// setTimeout(timeOut,3000)
+
+var min = 0;
+var sec = 0;
+var msec = 0;
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var interval;
+
+function reset(){
+        secHeading.innerHTML =0;
+        minHeading.innerHTML =0;
+        msecHeading.innerHTML =0;
+}
+function stop(){
+
+}
+function start(){
+
+    function timer(){
+  
+        msec++
+        msecHeading.innerHTML=msec;
+        if (msec>=100)
+        {
+            sec++;
+            msec=0;
+            secHeading.innerHTML = sec;
+            
+        }
+        else if(sec>=60)
+        {
+            min++;
+            sec=0;
+            minHeading.innerHTML = min;
+        }
+    
+    }
+    interval = setInterval(timer,10);
+    var x = document.getElementById("stop").disabled = true;
+}
+
+function stop()
+{
+        clearInterval(interval);
+        var x = document.getElementById("stop").disabled = false;
+}
+
+
+
+
+
